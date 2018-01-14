baa918f16fb542bfba69ffb0b2f608e3fbb89284
Banul <banul2@wp.pl>
2018-01-14 22:57:52 +0100

    błąd rozwiązany

--- src/Components/VehicleLocationReturner.js
+++ src/Components/VehicleLocationReturner.js
@@ -3,10 +3,17 @@ import axios from 'axios';
 
 
 const vehicleLocationReturner = (props) => {
-    const URL = "https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=%20f2e5503e-%20927d-4ad3-9500-4ab9e55deb59&apikey=0f3b3264-6613-4b0b-9f88-c7f84f770563&type=1&line="
+    const URL = "https://cors-anywhere.herokuapp.com/http://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=%20f2e5503e-%20927d-4ad3-9500-4ab9e55deb59&apikey=0f3b3264-6613-4b0b-9f88-c7f84f770563&type=1&line=132"
 
     return(
-        props.map(vehicleNumber => axios.get(`${URL}${vehicleNumber}`))
+        props.map(vehicleNumber => fetch(`${URL}`
+    ).then(results =>{
+
+            console.log(results.json());
+        } 
+    
+        )
+        )
     );
 }
 
